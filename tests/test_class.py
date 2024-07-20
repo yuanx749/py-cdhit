ad3c14aae0618a7bae8229b0ae6658f723654b7d
yuanx749 <yuanx749@gmail.com>
2024-07-20 18:42:22 +0800

    Improve tests

--- tests/test_class.py
+++ tests/test_class.py
@@ -1,4 +1,4 @@
-from subprocess import CalledProcessError
+from pathlib import Path
 
 import pandas as pd
 import pytest
@@ -7,6 +7,8 @@ from pycdhit import _class
 
 
 class TestCDHIT:
+    cdhit_installed = Path("~/cd-hit/cd-hit-est").expanduser().exists()
+
     @pytest.fixture(autouse=True)
     def init_cdhit(self):
         self.cdhit = _class.CDHIT(prog="cd-hit-est", path="~/cd-hit")
@@ -15,17 +17,16 @@ class TestCDHIT:
         with pytest.raises(ValueError):
             _class.CDHIT(prog="")
 
+    @pytest.mark.skipif(not cdhit_installed, reason="FileNotFoundError")
     def test_help(self, capsys):
-        try:
-            self.cdhit.help()
-            assert len(capsys.readouterr().out) > 0
-        except FileNotFoundError:
-            pass
+        self.cdhit.help()
+        assert len(capsys.readouterr().out) > 0
 
     def test_set_options(self):
         self.cdhit.set_options(c=0.9, n=10)
         assert self.cdhit.options == {"c": 0.9, "n": 10}
 
+    @pytest.mark.skipif(not cdhit_installed, reason="FileNotFoundError")
     def test_cluster_return_type(self):
         assert self.cdhit.options == {}
         fasta_in = pd.DataFrame(
@@ -34,9 +35,6 @@ class TestCDHIT:
                 "sequence": ["ACGTA", "CGTC", "CCGCC"],
             }
         )
-        try:
-            fasta, clstr = self.cdhit.cluster(fasta_in)
-            assert isinstance(fasta, pd.DataFrame)
-            assert isinstance(clstr, pd.DataFrame)
-        except (CalledProcessError, FileNotFoundError):
-            pass
+        fasta, clstr = self.cdhit.cluster(fasta_in)
+        assert isinstance(fasta, pd.DataFrame)
+        assert isinstance(clstr, pd.DataFrame)
